f832fd6064ef9148caa4a1d3b56f636c29b91bed
Torvik777 <79712569+Torvik777@users.noreply.github.com>
2022-04-19 16:23:08 +0300

    first git version

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function ()
+{
+    $('.menu-open').click(function()
+    {
+        $('.menu').toggleClass('show-menu')
+        $('.menu-div').toggleClass('show-menu-div')
+        $('.languages').toggleClass('show-languages')
+        
+    })
+
+});
